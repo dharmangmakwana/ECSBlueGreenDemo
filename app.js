681a7ddd9569be3fb6976f6068f3243055805897
dharmangmakwana <93462266+dharmangmakwana@users.noreply.github.com>
2023-08-10 22:34:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send("Hello World..!"))
+app.get('/', (req, res) => res.send("Hello World..! : Version 1.0.0"))
 app.listen(3000, () => console.log('Server ready'))
